0512c4f1d8f3073f16a0c7a6af83774a089dc210
daveotengo <30934250+daveotengo@users.noreply.github.com>
2023-07-23 23:34:25 +0000

    Initial Commit

---
+++ SweaterShop/SweaterShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopApp.swift
+//  SweaterShop
+//
+//  Created by David Oteng on 23/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
